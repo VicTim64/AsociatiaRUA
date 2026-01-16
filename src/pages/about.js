825aa756d19f73f49829d0723f0e1e2e6714c786
Vlad Olareanu <vlad.olareanu@gmail.com>
2026-01-16 12:48:21 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -65,7 +65,7 @@ import { useCallback } from "react";
             <div className="neoh_fn_team">
               <ul className="team_list">
                 {TeamInfor.map((item, key) =>
-                  key < 4 && (
+                  key < 8 && (
                     <li className="team_item" key={key}>
                       <div className="t_item">
                         <div className="person_info">
