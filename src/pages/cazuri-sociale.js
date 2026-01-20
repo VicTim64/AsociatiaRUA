7acfbcd2cd94f8221e0a68ab93ae0bb10a44a061
Vlad Olareanu <vlad.olareanu@gmail.com>
2026-01-20 04:43:52 +0200

    Update cazuri-sociale.js

--- src/pages/cazuri-sociale.js
+++ src/pages/cazuri-sociale.js
@@ -1,42 +1,37 @@
 import Layout from "@/layout/Layout";
 import PageBanner from "@/layout/PageBanner";
 import CazuriSociale from "@/components/CazuriSociale";
+import VideoPopup from "@/components/popup/VideoPopup";
 
 const CazuriSocialePage = () => {
   return (
     <Layout pageName="Cazuri Sociale">
       <PageBanner
         pageName="Cazuri Sociale"
-        imageURL="img/hero/head_MAAS_background.png"
+        imageURL="/img/hero/cazuri_background.png"
       />
 
       {/* Video sub titlu */}
       <div className="neoh_fn_video">
         <div className="bg_overlay">
-          <a
-            href="/video/cazuri_video.mp4"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <video
-              className="bg_video"
-              src="/img/MAAS/MAAS_movie.mp4"
-              autoPlay
-              loop
-              muted
-              playsInline
-              style={{ width: "100%", height: "100%", objectFit: "cover" }}
-            />
-          </a>
+          <video
+            className="bg_video"
+            src="/video/cazuri_video.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            onClick={() => VideoPopup.open("/video/cazuri_video.mp4")}
+          />
         </div>
         <div className="v_content">
-          <a
-            href="/video/cazuri_video.mp4"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src="svg/play.svg" alt="" className="fn__svg" />
-          </a>
+          <img
+            src="svg/play.svg"
+            alt=""
+            className="fn__svg"
+            onClick={() => VideoPopup.open("/video/cazuri_video.mp4")}
+          />
         </div>
       </div>
 
@@ -47,3 +42,4 @@ const CazuriSocialePage = () => {
 };
 
 export default CazuriSocialePage;
+
