867dacbaa546c632c20e0543efc4850cc8b58980
Vlad Olareanu <vlad.olareanu@gmail.com>
2026-01-20 05:18:25 +0200

    Update VideoPopupLocal.js

--- src/components/popup/VideoPopupLocal.js
+++ src/components/popup/VideoPopupLocal.js
@@ -6,28 +6,72 @@ const VideoPopupLocal_ = ({ close, videoSrc }) => {
 
   return (
     <Fragment>
-      <div className="mfp-bg mfp-ready" onClick={() => close(false)}></div>
+      {/* Background overlay */}
+      <div
+        className="mfp-bg mfp-ready"
+        onClick={() => close(false)}
+        style={{ cursor: "pointer" }}
+      ></div>
+
+      {/* Popup container */}
       <div
         className="mfp-wrap mfp-close-btn-in mfp-auto-cursor mfp-ready"
         tabIndex={-1}
-        style={{ overflow: "hidden auto" }}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden auto",
+        }}
       >
         <div className="mfp-container mfp-s-ready mfp-iframe-holder">
-          <div className="mfp-content" ref={domNode}>
-            <div className="mfp-iframe-scaler">
+          <div
+            className="mfp-content"
+            ref={domNode}
+            style={{
+              maxWidth: "90vw",
+              maxHeight: "90vh",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div className="mfp-iframe-scaler" style={{ width: "100%", height: "100%" }}>
+              {/* Close button */}
               <button
                 title="Close (Esc)"
                 type="button"
                 className="mfp-close"
                 onClick={() => close(false)}
+                style={{
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  zIndex: 1000,
+                  fontSize: "30px",
+                  background: "transparent",
+                  border: "none",
+                  color: "white",
+                  cursor: "pointer",
+                }}
               >
                 ×
               </button>
+
+              {/* Video */}
               <video
                 src={videoSrc}
                 controls
                 autoPlay
-                style={{ width: "100%", maxHeight: "80vh" }}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  maxHeight: "90vh",
+                  borderRadius: "10px",
+                  objectFit: "contain",
+                }}
               />
             </div>
           </div>
@@ -43,10 +87,14 @@ const VideoPopupLocal = ({ videoSrc, trigger }) => {
 
   return (
     <>
+      {/* Trigger */}
       <div onClick={() => setOpen(true)}>{trigger}</div>
+
+      {/* Popup */}
       {open && <VideoPopupLocal_ close={() => setOpen(false)} videoSrc={videoSrc} />}
     </>
   );
 };
 
 export default VideoPopupLocal;
+
