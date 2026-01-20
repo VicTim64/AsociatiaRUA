f4d105e6ae75eb688e258c21762100d889c83fba
Vlad Olareanu <vlad.olareanu@gmail.com>
2026-01-20 03:13:54 +0200

    Update WhyChooseUsComponent.js

--- src/components/WhyChooseUsComponent.js
+++ src/components/WhyChooseUsComponent.js
@@ -1,9 +1,27 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import landing_infor from "@/data/landing.json";
+// import partners_infor from "@/data/partners.json";
 
 const WhyChooseUsComponent = () => {
+  // Define styles for the logo container and the image itself
+  const logoContainerStyles = {
+    width: "200px",
+    height: "200px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "0 auto",
+  };
+
+  const logoStyles = {
+    maxWidth: "100%",
+    maxHeight: "100%",
+    objectFit: "contain",
+    display: "block",
+  };
+
   return (
     <section id="investor">
       <div className="container">
@@ -13,41 +31,64 @@ const WhyChooseUsComponent = () => {
 
         <Swiper
           className="neoh_fn_services"
-          modules={[Autoplay]}
+          spaceBetween={1}
+          slidesPerView={8}
           loop={true}
-          slidesPerView={4}
-          slidesPerGroup={1}   // ⭐ CHEIA
-          spaceBetween={40}
-          speed={800}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
+          modules={[Autoplay]}
+          autoplay={{ delay: 0, disableOnInteraction: false }}
+          freeMode={true}
+          speed={5000}
           breakpoints={{
-            320: { slidesPerView: 1 },
-            640: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
-            1280: { slidesPerView: 4 },
+            320: { slidesPerView: 1, spaceBetween: 20 },
+            400: { slidesPerView: 1, spaceBetween: 40 },
+            480: { slidesPerView: 1, spaceBetween: 40 },
+            640: { slidesPerView: 2, spaceBetween: 40 },
+            768: { slidesPerView: 2, spaceBetween: 50 },
+            1024: { slidesPerView: 3, spaceBetween: 50 },
+            1280: { slidesPerView: 3, spaceBetween: 50 },
           }}
         >
-          {landing_infor.map((item, index) => (
-            <SwiperSlide key={index}>
+          {landing_infor.map((index, key) => (
+            <SwiperSlide key={key}>
               <img
-                src={`img/landing/${item.avatar}`}
+                src={`img/landing/${index.avatar}`}
                 alt="avatar"
-                style={{
-                  width: "100%",
-                  maxHeight: "200px",
-                  objectFit: "contain",
-                }}
               />
             </SwiperSlide>
           ))}
         </Swiper>
+
+        {/*
+        <div className="neoh_fn_title">
+          <h3 className="fn_title" style={{ marginTop: "100px" }}>
+            Partners
+          </h3>
+        </div>
+        */}
+
+        {/*
+        <div className="neoh_fn_investor">
+          <ul>
+            {partners_infor.map((index, key) => (
+              <li key={key}>
+                <div className="item">
+                  <div style={logoContainerStyles}>
+                    <img
+                      src={`img/landing/${index.logo}`}
+                      alt={index.title}
+                      style={logoStyles}
+                    />
+                  </div>
+                  <a href={index.url} className="full_link" />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        */}
       </div>
     </section>
   );
 };
 
 export default WhyChooseUsComponent;
-
