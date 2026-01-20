8bd01e11896b8ca8e86b2a13072a2609185b0dba
Vlad Olareanu <vlad.olareanu@gmail.com>
2026-01-20 04:30:48 +0200

    Update cazuri-sociale.js

--- src/pages/cazuri-sociale.js
+++ src/pages/cazuri-sociale.js
@@ -9,6 +9,38 @@ const CazuriSocialePage = () => {
         pageName="Cazuri Sociale"
         imageURL="/img/hero/cazuri_background.png"
       />
+
+      {/* Video sub titlu */}
+      <div className="neoh_fn_video">
+        <div className="bg_overlay">
+          <a
+            href="/video/cazuri_video.mp4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <video
+              className="bg_video"
+              src="/video/cazuri_video.mp4"
+              autoPlay
+              loop
+              muted
+              playsInline
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </a>
+        </div>
+        <div className="v_content">
+          <a
+            href="/video/cazuri_video.mp4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="svg/play.svg" alt="" className="fn__svg" />
+          </a>
+        </div>
+      </div>
+
+      {/* Componenta cu cazuri */}
       <CazuriSociale />
     </Layout>
   );
